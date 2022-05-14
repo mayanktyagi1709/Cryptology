ef4eb60f492029506470f913a2f996e7521dff30
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2022-05-14 12:33:33 +0530

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+import { Button, Menu, Typography, Avatar } from "antd"
+import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from "@ant-design/icons"
+
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      
+    </div>
+  )
+}
+
+export default Navbar
