b8b8a55c7aa4b3059c03202fce4b79442910f999
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2022-05-15 22:22:29 +0530

    Created Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import { Navbar }  from './components';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <div className="navbar">
-
+        <Navbar/>
       </div>
       <div className="main"></div>
       <div className="footer"></div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,40 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import { Button, Menu, Typography, Avatar } from "antd"
-import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from "@ant-design/icons"
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Menu, Typography, Avatar } from "antd";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
 
 const Navbar = () => {
   return (
     <div className="nav-container">
-      
-    </div>
-  )
-}
+      <div className="logo-container">
+        <Avatar src={""} size="large" />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Cryptology</Link>
+        </Typography.Title>
 
-export default Navbar
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
