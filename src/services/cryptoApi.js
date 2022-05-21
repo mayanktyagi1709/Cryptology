eb659c7c1e0e43b4fed725b860ca303e4c48b7ae
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2022-05-21 11:58:26 +0530

    Created Homepage

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+  "X-RapidAPI-Key": "d761d102e5msh3f60b8b59397920p19ba8ajsn1f76d8168494",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+          query: () => createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery
+  } = cryptoApi;
+  
